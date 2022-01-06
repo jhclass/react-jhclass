36a3ce0330964e94e5d2f2e019adddf81f4a5cc2
jhclass <jhcode@naver.com>
2022-01-06 17:29:35 +0900

    html 문서에서 App.js 로 옮기기

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //wrap 이 만약에 없다면?? 에러가 뜹니다! 이유는? 
+    //리액트는 가장 상위 태그는 하나만 존재해야한다는 사실~!
+    <div id="wrap">
+      
+      <div id="title">
+          <h1>FAQ</h1>
+          <p>자주 묻는 질문입니다.</p>
+          {/* 타이틀 jsx 주석 */}
+      </div>
+      <div id="faqList">
+        <ul>
+            <li><a href="#">자주묻는질문1</a></li>
+            <li><a href="#">자주묻는질문2</a></li>
+            <li><a href="#">자주묻는질문3</a></li>
+        </ul>
+      </div>
+      <div id="content">
+          <h2>자주묻는질문1</h2>
+          <p>자주묻는질문1에 대한 내용이 여기에 포함됩니다.</p>
+      </div>
     </div>
+    
   );
 }
 
