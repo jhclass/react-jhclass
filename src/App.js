6f640a449be1559b389ef307e9b4657bbc20bfb6
jhclass <jhcode@naver.com>
2022-01-11 15:45:51 +0900

    React: State? 01.기초

--- src/App.js
+++ src/App.js
@@ -7,18 +7,27 @@ import './App.css';
 
 
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super(props);//초기화
+    //state설정
+    this.state={
+      headingTitle:{title:'FAQ',desc:'자주 묻는 질문입니다.'}
+    }
+  }
+  render(){
   return (
     //wrap 이 만약에 없다면?? 에러가 뜹니다! 이유는? 
     //리액트는 가장 상위 태그는 하나만 존재해야한다는 사실~!
     <div id="wrap">
       
-      <Title title='FAQ' desc='자주 묻는 질문입니다.'></Title>
+      <Title title={this.state.headingTitle.title} desc={this.state.headingTitle.desc}></Title>
       <FaqList></FaqList>
       <Content></Content>
     </div>
     
   );
+  }
 }
 
 export default App;
